30424c0fdacd53787d1acdbc54b12b9f039f9ff1
Rinka <146360541+Rinka-Dokuta@users.noreply.github.com>
2024-09-09 10:07:18 -0600

    Create for loops quiz.py

---
+++ for loops quiz.py
@@ -0,0 +1,19 @@
+for i in range(0, 19): # for loop that prints out the numbers 0-18. 
+    print(i, end = " ")
+    
+print()
+    
+for i in range(25, 46): #for loop that prints out the numbers 25-45.
+    print(i, end = " ")
+    
+print()
+
+for i in range(40, 84, 4): #for loop that prints out the numbers 40 through 80 counting by 4s
+    print(i, end = " ")
+    
+print()
+    
+for i in range(12, -9, -3): #for loop that prints out the numbers 12 to -6 counting down by -3s
+    print(i, end = " ")
+
+print()
